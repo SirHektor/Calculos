f447520330d151b55cc8b942241c08421a1579ec
SirHektor <heitorcosta.aredes@Gmail.com>
2023-08-03 16:46:11 -0300

    Arrumei o ret do js e comecei a fazer contas novas

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function Ret(){
     alert(res);
     document.getElementById('a_ret').innerHTML=res
 }
+function Ret2(){
+    let x=Number(prompt("Insira o primeiro lado da forma"))
+    let y=Number(prompt("Insira o segundo lado da forma"))
+    let z=Number(prompt("Insira o terceiro lado da forma"))
+    res='O volume desta forma é '+x*y*z;
+    alert(res);
+    document.getElementById('b_ret').innerHTML=res
+}
+function Ret3(){
+    let x=Number(prompt("Insira o primeiro lado da forma"))
+    let y=Number(prompt("Insira o segundo lado da forma"))
+    let z=Number(prompt("Insira o terceiro lado da forma"))
+    res='O volume desta forma é '+x*y*z;
+    alert(res);
+    document.getElementById('c_ret').innerHTML=res
+}
+function Ret4(){
+    let x=Number(prompt("Insira o primeiro lado da forma"))
+    let y=Number(prompt("Insira o segundo lado da forma"))
+    let z=Number(prompt("Insira o terceiro lado da forma"))
+    res='O volume desta forma é '+x*y*z;
+    alert(res);
+    document.getElementById('d_ret').innerHTML=res
+}
